dc549f5888d05258c1ad6822495f263f9b190903
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 20:44:18 -0300

    Update EmentaView.java

--- TrabalhoPOO/src/view/EmentaView.java
+++ TrabalhoPOO/src/view/EmentaView.java
@@ -1,4 +1,14 @@
 package view;
-
+import controller.EmentaController;
+import model.Model;
 public class EmentaView {
+  private Model model;
+  private EmentaController controller;
+
+  public void init(Model model){
+    this.model = model;
+    this.controller = new EmentaController;
+    
+  }
+    
 }
