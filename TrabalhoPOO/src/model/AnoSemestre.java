2a724c7e070e3628ce2c56e6f0c78d4ea33ec8f8
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:26:37 -0300

    Update AnoSemestre.java

--- TrabalhoPOO/src/model/AnoSemestre.java
+++ TrabalhoPOO/src/model/AnoSemestre.java
@@ -1,22 +1,22 @@
 package model;
 
-public class AnoSemestre {
+class AnoSemestre {
     private int ano;
     private int semestre;
 
-    public int getAno() {return ano;}
+    public int getAno() { return ano; }
 
     public void setAno(int ano) {
-        if(ano > 1900){
-            this.ano = ano;
+        if (ano > 1900) {
+            this.ano = ano; // Define o ano se for válido
         }
     }
 
-    public int getSemestre() {return semestre;}
+    public int getSemestre() { return semestre; }
 
     public void setSemestre(int semestre) {
-        if(semestre == 1 || semestre == 2) {
-            this.semestre = semestre;
+        if (semestre == 1 || semestre == 2) {
+            this.semestre = semestre; // Define o semestre como 1 ou 2
         }
     }
 }
