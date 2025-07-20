f72cb01f959b15e90f20d32fa49e007728009e07
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:11:23 -0300

    Update BibliografiaController.java

--- TrabalhoPOO/src/controller/BibliografiaController.java
+++ TrabalhoPOO/src/controller/BibliografiaController.java
@@ -1,4 +1,49 @@
 package controller;
 
-public class BibliografiaController {
+import view.*;
+import model.Model;
+
+// Controller que gerencia ações da bibliografia (material da turma)
+public class BibliografiaController implements Observer {
+    private Model model;
+    private BibliografiaView view;
+
+    public void init(Model model, BibliografiaView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    // Reage a eventos como adicionar, excluir material ou finalizar
+    public void handleEvent(String event) {
+        String codigoTurma = view.getCodigoTurma();
+        String idMaterial = view.getIdMaterial();
+        String titulo = view.getTitulo();
+        String autor = view.getAutor();
+        String ano = view.getAno();
+        String isbn = view.getIsbn();
+
+        switch (event) {
+            case "1": // Adicionar material
+                if(model.existeMaterial(codigoTurma, idMaterial)){
+                    view.exibeMSG("ERRO: Esse ID de Material já existe!");
+                } else {
+                    model.addMaterial(codigoTurma, idMaterial, titulo, autor, ano, isbn);
+                }
+                break;
+            case "2": // Excluir material
+                if(model.existeMaterial(codigoTurma, idMaterial)){
+                    model.excluirMaterial(codigoTurma, idMaterial);
+                } else{
+                    view.exibeMSG("ERRO: Esse material não existe!");
+                }
+                break;
+            case "0": // Finalizar view
+                view.finalizar();
+                break;
+        }
+    }
+
+    public void update(){
+
+    }
 }
