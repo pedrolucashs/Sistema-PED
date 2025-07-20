809204165d079071ec2a3634138e5f1dce1d0c4f
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:47:53 -0300

    Update Professor.java

--- TrabalhoPOO/src/model/Professor.java
+++ TrabalhoPOO/src/model/Professor.java
@@ -2,35 +2,42 @@ package model;
 
 import java.util.HashMap;
 
-public class Professor extends Usuario {
-    private HashMap<String, Turma> turmas;
-    private Campus campus;
+// Classe que representa um professor, herdando de Usuario
+class Professor extends Usuario {
+    private HashMap<String, Turma> turmas; // Mapeamento de turmas atribuídas ao professor (chave: ID ou código da turma)
+    private Campus campus; // Campus onde o professor atua
 
+    // Construtor da classe Professor
     public Professor(String nome, String id, String login, String senha, Campus campus) {
-        super(nome, id, login, senha);
-        setTurmas(new HashMap<String,Turma>());
-        setCampus(campus);
+        super(nome, id, login, senha); // Chamada do construtor da superclasse Usuario
+        setTurmas(new HashMap<String, Turma>()); // Inicializa o mapa de turmas como vazio
+        setCampus(campus); // Define o campus
     }
 
     public HashMap<String, Turma> getTurmas() {
         return turmas;
     }
+     // Define o mapa de turmas, se não for nulo
     public void setTurmas(HashMap<String, Turma> turmas) {
-        if(turmas != null) {
+        if (turmas != null) {
             this.turmas = turmas;
         }
     }
 
+    // Retorna o campus do professor
     public Campus getCampus() {
         return campus;
     }
+
+    // Define o campus, se não for nulo
     public void setCampus(Campus campus) {
-        if(campus != null) {
+        if (campus != null) {
             this.campus = campus;
         }
     }
 
-    public String toString(){
-        return String.format("nome: %s\nLogin: %s\nId: %s",getNome(),getLogin(),getId());
+    // Representação textual do professor (nome, login e ID)
+    public String toString() {
+        return String.format("nome: %s\nLogin: %s\nId: %s", getNome(), getLogin(), getId());
     }
 }
