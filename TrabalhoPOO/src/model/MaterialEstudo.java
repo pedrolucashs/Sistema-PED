6909c6c9ca0f9800ad263a7db87c95cff71ab9c2
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 23:22:29 -0300

    Update MaterialEstudo.java

--- TrabalhoPOO/src/model/MaterialEstudo.java
+++ TrabalhoPOO/src/model/MaterialEstudo.java
@@ -1,48 +1,61 @@
 package model;
-// Classe que representa um material de estudo (ex: livro)
 
 public class MaterialEstudo {
+    private String idMaterial;
     private String titulo;
     private String autor;
-    private String edicao;
-    private String editora;
-    private int ano;
+    private String ano;
     private String isbn;
 
-    public String getTitulo() {return titulo;}
+    public MaterialEstudo(String idMaterial, String titulo, String autor, String ano, String isbn) {
+        setIdMaterial(idMaterial);
+        setTitulo(titulo);
+        setAutor(autor);
+        setAno(ano);
+        setIsbn(isbn);
+    }
 
+    public String getIdMaterial() {
+        return idMaterial;
+    }
+    public void setIdMaterial(String idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public String getTitulo() {return titulo;}
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
     public String getAutor() {return autor;}
-
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public String getEdicao() {return edicao;}
-
-    public void setEdicao(String edicao) {
-        this.edicao = edicao;
+    public String getAno() {
+        return ano;
     }
-
-    public String getEditora() {return editora;}
-
-    public void setEditora(String editora) {
-        this.editora = editora;
-    }
-
-    public int getAno() {return ano;}
-
-    public void setAno(int ano) {
+    public void setAno(String ano) {
         this.ano = ano;
     }
 
     public String getIsbn() {return isbn;}
-
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
+    public String toString() {
+        if(idMaterial == null) {
+            return "";
+        }
+        String s = String.format("Id do Material: %s\nTítulo: %s | Autor: %s\nAno: %s | ISBN: %s", getIdMaterial(), getTitulo(), getAutor(), getAno(), getIsbn());
+        return s;
+    }
+    public String toString2() {
+        if(idMaterial == null) {
+            return "";
+        }
+        String s = String.format("Título: %s | Autor: %s\nAno: %s | ISBN: %s", getIdMaterial(), getTitulo(), getAutor(), getAno(), getIsbn());
+        return s;
+    }
 }
