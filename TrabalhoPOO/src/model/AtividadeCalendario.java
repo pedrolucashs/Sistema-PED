f85171ac661532addbc6616ab781cbfaf07e0388
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:27:53 -0300

    Update AtividadeCalendario.java

--- TrabalhoPOO/src/model/AtividadeCalendario.java
+++ TrabalhoPOO/src/model/AtividadeCalendario.java
@@ -1,27 +1,19 @@
-package model;
-
-public class AtividadeCalendario {
+// Classe que representa uma atividade do calendário escolar
+class AtividadeCalendario {
     private String data;
     private String descricaoConteudo;
     private int cargaHoraria;
 
-    public String getData() {return data;}
+    public String getData() { return data; }
+    public void setData(String data) { this.data = data; }
 
-    public void setData(String data) {
-        this.data = data;
-    }
+    public String getDescricaoConteudo() { return descricaoConteudo; }
+    public void setDescricaoConteudo(String descricaoConteudo) { this.descricaoConteudo = descricaoConteudo; }
 
-    public String getDescricaoConteudo() {return descricaoConteudo;}
-
-    public void setDescricaoConteudo(String descricaoConteudo) {
-        this.descricaoConteudo = descricaoConteudo;
-    }
-
-    public int getCargaHoraria() {return cargaHoraria;}
-
+    public int getCargaHoraria() { return cargaHoraria; }
     public void setCargaHoraria(int cargaHoraria) {
-        if(cargaHoraria > 0){
-            this.cargaHoraria = cargaHoraria;
+        if (cargaHoraria > 0) {
+            this.cargaHoraria = cargaHoraria; // Define a carga horária se for positiva
         }
     }
 }
