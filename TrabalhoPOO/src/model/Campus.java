ac16e96a5e5fa9195e4268664632b7fd399b84d8
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:28:50 -0300

    Update Campus.java

--- TrabalhoPOO/src/model/Campus.java
+++ TrabalhoPOO/src/model/Campus.java
@@ -1,7 +1,8 @@
 package model;
 import java.util.HashMap;
 
-public class Campus {
+// Classe que representa um campus contendo professores e turmas
+class Campus {
     private String nomeUnidade;
     private HashMap<String, Professor> professores;
     private HashMap<String, Turma> turmas;
@@ -12,26 +13,20 @@ public class Campus {
         setProfessores(professores);
     }
 
-    public String getNomeUnidade() {return nomeUnidade;}
-    public void setNomeUnidade(String nomeUnidade) {
-        this.nomeUnidade = nomeUnidade;
-    }
+    public String getNomeUnidade() { return nomeUnidade; }
+    public void setNomeUnidade(String nomeUnidade) { this.nomeUnidade = nomeUnidade; }
 
-    public HashMap<String, Professor> getProfessores() {
-        return professores;
-    }
+    public HashMap<String, Professor> getProfessores() { return professores; }
     public void setProfessores(HashMap<String, Professor> professores) {
         if (professores != null) {
-            this.professores = professores;
+            this.professores = professores; // Define os professores se não for nulo
         }
     }
 
-    public HashMap<String, Turma> getTurmas() {
-        return turmas;
-    }
+    public HashMap<String, Turma> getTurmas() { return turmas; }
     public void setTurmas(HashMap<String, Turma> turmas) {
-        if(turmas != null) {
-            this.turmas = turmas;
+        if (turmas != null) {
+            this.turmas = turmas; // Define as turmas se não for nulo
         }
     }
 }
