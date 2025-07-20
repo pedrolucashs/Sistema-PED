e8bdb2596668bcf98860ef6e5b7cd21708173cf2
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:11:39 -0300

    Update CadastrarProfController.java

--- TrabalhoPOO/src/controller/CadastrarProfController.java
+++ TrabalhoPOO/src/controller/CadastrarProfController.java
@@ -3,6 +3,7 @@ package controller;
 import view.*;
 import model.Model;
 
+// Controller para cadastrar professor, responde a eventos da view
 public class CadastrarProfController implements Observer {
     private Model model;
     private CadastrarProfView view;
@@ -10,18 +11,18 @@ public class CadastrarProfController implements Observer {
     public void init(Model model, CadastrarProfView view) {
         this.model = model;
         this.view = view;
-        model.attachObserver(this);
+        model.attachObserver(this); // Se inscreve para receber updates do model
     }
     public void handleEvent(String event) {
         switch (event) {
-            case "OK" :
+            case "OK" : // Quando usuário confirma cadastro
                 boolean cadastrado = model.setUsuario(view.getNome(), view.getId(), view.getLogin(),view.getSenha());
                 if(cadastrado){
                     view.exibeMSG("SUCESSO: Professor cadastrado!");
                 } else{
                     view.exibeMSG("ERRO: Esse nome de usuário já está sendo usado.");
                 }
-                model.detachObserver(this);
+                model.detachObserver(this); // Para de ouvir o model depois do cadastro
                 break;
         }
     }
