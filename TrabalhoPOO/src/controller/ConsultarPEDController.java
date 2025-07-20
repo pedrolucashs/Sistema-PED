7bee0da7de8cb74af4a7e2640e72ab91b4869174
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:12:27 -0300

    Update ConsultarPEDController.java

--- TrabalhoPOO/src/controller/ConsultarPEDController.java
+++ TrabalhoPOO/src/controller/ConsultarPEDController.java
@@ -3,6 +3,7 @@ package controller;
 import view.*;
 import model.Model;
 
+// Controller para consultar o Plano de Ensino (PED)
 public class ConsultarPEDController implements Observer{
     Model model;
     ConsultarPEDView view;
@@ -13,6 +14,7 @@ public class ConsultarPEDController implements Observer{
         model.attachObserver(this);
     }
 
+    // Quando o usuário confirma, verifica se o PED existe para a turma e unidade
     public void handleEvent(String event) {
         switch(event){
             case "OK":
@@ -21,6 +23,7 @@ public class ConsultarPEDController implements Observer{
                     view.exibeMSG("ERRO: Talvez Campus e/ou turma estejam errados! Ou esse Plano de Ensino ainda não existe.");
                 } else{
                     view.exibeMSG("SUCESSO: Plano de Ensino encontrado!");
+                    // Abre a view para visualizar o PED
                     VisualizarPEDView view2 = new VisualizarPEDView();
                     view2.init(model, view.getCodigoTurma(), view.getNomeUnidade());
                 }
