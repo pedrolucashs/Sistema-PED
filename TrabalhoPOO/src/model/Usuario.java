f69ceeaae7a5f9835c681310b78649cc5ad4fbd0
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:52:26 -0300

    Update Usuario.java

--- TrabalhoPOO/src/model/Usuario.java
+++ TrabalhoPOO/src/model/Usuario.java
@@ -1,11 +1,24 @@
-package model;
+ackage model;
 
+/**
+ * Classe que representa um usuário genérico do sistema.
+ * Pode ser estendida para representar diferentes tipos de usuários (como Professor, Admin).
+ */
 public class Usuario {
+    // Nome completo do usuário
     private String nome;
+
+    // Identificador único do usuário (pode ser matrícula, CPF, etc)
     private String id;
+
+    // Login usado para autenticação
     private String login;
+
+    // Senha usada para autenticação
     private String senha;
 
+    // Construtor da classe Usuario
+    
     public Usuario(String nome, String id, String login, String senha){
         setNome(nome);
         setId(id);
@@ -13,9 +26,13 @@ public class Usuario {
         setSenha(senha);
     }
 
+    //Retorna o nome do usuário
+
     public String getNome() {
         return nome;
     }
+
+    // Define o nome do usuário (caso não seja nulo)
 
     public void setNome(String nome) {
         if (nome != null) {
@@ -23,33 +40,46 @@ public class Usuario {
         }
     }
 
+    // Retorna o login do usuário
+
     public String getLogin() {
         return login;
     }
 
+    // Define o login do usuário (caso não seja nulo)
+    
     public void setLogin(String login) {
         if (login != null) {
             this.login = login;
         }
     }
 
+    // Retorna a senha do usuário
+     
     public String getSenha() {
         return senha;
     }
 
+    // Define a senha do usuário (caso não seja nula)
+    
     public void setSenha(String senha) {
         if (senha != null) {
             this.senha = senha;
         }
     }
 
+    //Retorna o ID do usuário
     public String getId() {
         return id;
     }
+
+    //Define o ID do usuário
     public void setId(String id) {
         this.id = id;
     }
 
+    // Retorna uma representação textual simples do usuário
+    
     public String toString() {
         return nome + " " + login;
     }
