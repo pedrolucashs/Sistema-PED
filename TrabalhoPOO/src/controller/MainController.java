f460e0f81f176be51317f0b07fb883da6d542c99
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:18:03 -0300

    Update MainController.java

--- TrabalhoPOO/src/controller/MainController.java
+++ TrabalhoPOO/src/controller/MainController.java
@@ -3,6 +3,7 @@ package controller;
 import model.Model;
 import view.*;
 
+// Controller principal do sistema, que abre as views iniciais (login, consultar PED)
 public class MainController implements Observer{
     private Model model;
     private MainView view;
@@ -14,17 +15,18 @@ public class MainController implements Observer{
         }
     }
 
+    // Trata evento da tela principal (menu inicial)
     public void handleEvent(String event) {
         switch (event) {
-            case "1" :
+            case "1" : // Abre a tela de login
                 LoginView view1 = new LoginView();
                 view1.init(model);
                 break;
-            case "2" :
+            case "2" : // Abre a tela de consulta do PED
                 ConsultarPEDView view2 = new ConsultarPEDView();
                 view2.init(model);
                 break;
-            case "0" : view.finalizarSistema(); break;
+            case "0" : view.finalizarSistema(); break; // Sai do sistema
         }
     }
 
