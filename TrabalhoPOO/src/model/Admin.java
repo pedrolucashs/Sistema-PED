9072a4efdd526641229eea4516e87cecef1dbd5d
Bia  Gomes <12beatriz.menezes@gmail.com>
2025-07-19 22:26:02 -0300

    Update Admin.java

--- TrabalhoPOO/src/model/Admin.java
+++ TrabalhoPOO/src/model/Admin.java
@@ -1,18 +1,20 @@
 package model;
 
+// Classe que representa um administrador do sistema, herdando de Usuario
 public class Admin extends Usuario {
-    private Campus unidade;
+    private Campus unidade; // Unidade (campus) associada ao administrador
 
     public Admin(String nome, String id, String login, String senha) {
-        super(nome, id, login, senha);
+        super(nome, id, login, senha); // Chamada ao construtor da superclasse
     }
 
     public Campus getUnidade() {
-        return unidade;
+        return unidade; // Retorna o campus associado
     }
+
     public void setUnidade(Campus unidade) {
         if (unidade != null) {
-            this.unidade = unidade;
+            this.unidade = unidade; // Define o campus, se não for nulo
         }
     }
 }
