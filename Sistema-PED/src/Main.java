d557799c62fc51fd746de85b55f45497ca9357be
Pedro Lucas <pedrolucashs407@gmail.com>
2026-01-18 21:48:35 -0300

    Fix comment formatting in Main.java

--- Sistema-PED/src/Main.java
+++ Sistema-PED/src/Main.java
@@ -3,7 +3,7 @@ import view.*;
 
 public class Main {
     public static void main(String[] args) {
-        //O usuário padrão é: Nome de Usuário - Admin, Senha - Senha
+        //O usuário padrão é: Nome de Usuário - Admin, Senha - Senha.
         Model model = Model.getInstancia();
         MainView view = new MainView();
         view.init(model);
